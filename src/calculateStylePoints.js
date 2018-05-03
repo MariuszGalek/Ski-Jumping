1e5340bbde2eabf0a2cf4111200077d41e70af04
Mariusz Galek <mgalek1@gmail.com>
2018-05-03 13:30:37 +0200

    Add calculate style points function

--- src/calculateStylePoints.js
+++ src/calculateStylePoints.js
@@ -1,3 +1,8 @@
 const calculateStylePoints = (styleNotes) => {
-  ...
+	let max = Math.max(...styleNotes);
+	let min = Math.min(...styleNotes);
+
+	return styleNotes.reduce((a,b) => a+b ) - (max + min);
 };
+
+module.exports = calculateStylePoints;
